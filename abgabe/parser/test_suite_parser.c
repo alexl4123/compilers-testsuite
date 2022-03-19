72e75c76ed25472ce557d126bc1d6068363f6d02
alexl4123 <alexl.id.at@gmail.com>
2022-03-19 16:26:20 +0100

    [parser] - added test suite for parser

--- abgabe/parser/test_suite_parser.c
+++ abgabe/parser/test_suite_parser.c
@@ -8,19 +8,25 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#include <time.h>
+
+enum fail_state {SHOULD_WORK = 0, SCANNER_SHOULD_FAIL = 1, PARSER_SHOULD_FAIL = 2};
+
 /**
  * This function is used for forking, creating pipes and shutting down the scanner again.
  *
  * @input - The first string is the ''input'', so this is given to the scanner program.
  * @output - The second string is the ''should-output'', so what the response of the scanner, given the input should be (can be left blank in case of an error test case)
  * @should_fail - If set to one it is tested if the scanner program terminates with exit code 1
+ * @return - 1 if test case successful, 0 otherwise
  */
-int main_test_loop(char * input, char * output, int should_fail);
+int main_test_loop(char * input, int fail_case);
 
 /**
  * This function is used for input-output-checking.
  */
-int test_loop(int parent_child_pipe, int child_parent_pipe, char * input, char * output, int should_fail);
+int test_loop(int parent_child_pipe, int child_parent_pipe, char * input);
+
 
 /**
  * Main is just for test cases -> How to include test cases?
@@ -29,49 +35,79 @@ int test_loop(int parent_child_pipe, int child_parent_pipe, char * input, char *
 int main(int argc, char * argv[]) {
     int test_cases_executed = 0;
     int test_cases_successful = 0;
-    
+
     {
-        // Test 1:
-	test_cases_successful += main_test_loop("test", "id test\n",0);
-	test_cases_executed += 1;
+        // Test 1 (simple normal testcase):
+        test_cases_successful += main_test_loop("hallo (variable) end ;", SHOULD_WORK);
+        test_cases_executed += 1;
     }
     {
-        // Test 2:
-	test_cases_successful += main_test_loop("39end end39", "num 39\nend\nid end39\n",0);
-	test_cases_executed += 1;
+        // Test 2 (only id on file end should fail):
+        test_cases_successful += main_test_loop("hallo", PARSER_SHOULD_FAIL);
+        test_cases_executed += 1;
     }
     {
-        // Test 3:
-	test_cases_successful += main_test_loop("// kommentar", "",0);
-	test_cases_executed += 1;
+        // Test 3 (test 1 of assignment):
+        test_cases_successful += main_test_loop("f(x) end;", SHOULD_WORK);
+        test_cases_executed += 1;
     }
     {
-        // Test 4:
-	test_cases_successful += main_test_loop(";(){},:=[]+*>-@end return goto if var not and", ";\n(\n)\n{\n}\n,\n:\n=\n[\n]\n+\n*\n>\n-\n@\nend\nreturn\ngoto\nif\nvar\nnot\nand\n",0);
-	test_cases_executed += 1;
+        // Test 4 (test 2 of assignment):
+        test_cases_successful += main_test_loop("\n <%&\n", SCANNER_SHOULD_FAIL);
+        test_cases_executed += 1;
     }
     {
-        // Test 5:
-	test_cases_successful += main_test_loop("endreturngotoifvarnotand", "id endreturngotoifvarnotand\n",0);
-	test_cases_executed += 1;
+        // Test 5 (test 3 of assignment):
+        test_cases_successful += main_test_loop("f() end;", PARSER_SHOULD_FAIL);
+        test_cases_executed += 1;
     }
     {
-        // Test 6:
-	test_cases_successful += main_test_loop("$10 10", "num 16\nnum 10\n",0);
-	test_cases_executed += 1;
+        // Test 6 (tests stats and single stat term):
+        test_cases_successful += main_test_loop("f(x,y) hallo : hallo : hallo : $10; end;", SHOULD_WORK);
+        test_cases_executed += 1;
     }
     {
-        // Test 7:
-	test_cases_successful += main_test_loop("%", "",1);
-	test_cases_executed += 1;
+        // Test 7 (tests stat statements):
+        test_cases_successful += main_test_loop("f(x,y) $10; 11; end;", SHOULD_WORK);
+        test_cases_executed += 1;
     }
-    
+    {
+        // Test 8 (tests multiple stat statements):
+        test_cases_successful += main_test_loop("f(x,y) return $10; goto hallo; if 1 goto hallo; var hallo = 1; hallo = $1; $1[$1] = $1[$1]; end;", SHOULD_WORK);
+        test_cases_executed += 1;
+    }
+    {
+        // Test 9 (tests expr and multiple term):
+        test_cases_successful += main_test_loop("f(x,y) return not - - not not $10; return 10 + 10 + 12; return 10 * 10 * 12; return 10 and 10 and 12; return 10 > 12; return 10 = 12;  end;", SHOULD_WORK);
+        test_cases_executed += 1;
+    }
+    {
+        // Test 10 (tests multiple term):
+        test_cases_successful += main_test_loop("f(x,y) return 10; return hallo[5]; return hallo(a,b,c); return hallo{a,b,c,d}; return hallo@(a,b,c,d,e);  end;", SHOULD_WORK);
+        test_cases_executed += 1;
+    }
+    {
+        // Test 11 (higher order function):
+        test_cases_successful += main_test_loop("f{x,y,z}(x,y) end;", SHOULD_WORK);
+        test_cases_executed += 1;
+    }
+    {
+        // Test 12 (normal function fail):
+        test_cases_successful += main_test_loop("f(x,y,) end;", PARSER_SHOULD_FAIL);
+        test_cases_executed += 1;
+    }
+
+
+
+
+
+
     fprintf(stdout, "Total test cases executed: %d, successful: %d.\n",
-		    test_cases_executed,
-		    test_cases_successful);
+            test_cases_executed,
+            test_cases_successful);
 }
 
-int main_test_loop(char * input, char * output, int should_fail) {
+int main_test_loop(char * input, int fail_case) {
 
     int parent_child_pipe[2], child_parent_pipe[2];
     int ret = pipe(parent_child_pipe);
@@ -94,70 +130,63 @@ int main_test_loop(char * input, char * output, int should_fail) {
             // child -> start parser
             close(parent_child_pipe[1]); // close write end
             close(child_parent_pipe[0]); // close read end
-               
+
             dup2(parent_child_pipe[0], STDIN_FILENO);
             dup2(child_parent_pipe[1], STDOUT_FILENO);
 
-	    char * path_string = "../../../abgabe/scanner/scanner";
-            execlp(path_string, "scanner", NULL);
-            
-	    fprintf(stderr, "execlp failed with path: %s\n", path_string);
-	    exit(-1);
+            char * path_string = "../../../abgabe/parser/parser";
+            execlp(path_string, "parser", NULL);
+
+            fprintf(stderr, "execlp failed with path: %s\n", path_string);
+            exit(-1);
         default:
             // parent tasks
             close(parent_child_pipe[0]); // close read end
             close(child_parent_pipe[1]); // close write end
 
-            ret = test_loop(parent_child_pipe[1], child_parent_pipe[0], input, output, should_fail);           
+            ret = test_loop(parent_child_pipe[1], child_parent_pipe[0], input);
 
-	    if (should_fail == 0) {
-		if (kill(pid, SIGTERM) < 0) {
-	            fprintf(stderr, "sigterm failed! \n");
-                }
-	    } else {
-	        int status;
-		pid_t cur_pid;
-		while ((cur_pid = wait(&status)) != pid) {
-	            if (cur_pid != -1) continue;
-		    if (errno == EINTR) continue;
-		    fprintf(stderr, "Wait failed!\n");
-		    break;
-		}
-		if (cur_pid != -1 && WEXITSTATUS(status) == 1) {
-		    ret = 0;
-		}
+            int status;
+            pid_t cur_pid;
 
-	    }
-	               
+            while ((cur_pid = wait(&status)) != pid) {
+                if (cur_pid != -1) continue;
+                if (errno == EINTR) continue;
+                fprintf(stderr, "Wait failed!\n");
+                break;
+            }
+            fprintf(stdout, "Fail case: %d, exit status: %d \n",fail_case, WEXITSTATUS(status));
+            if (cur_pid != -1 && WEXITSTATUS(status) == fail_case) {
+                ret = 1;
+            } else {
+                ret = 0;
+            }
 
+            if (ret == 1) {
+                fprintf(stdout, "Test case successful with input: %s \n", input);
+                ret = 1;
+            } else {
+                fprintf(stdout, "Test case failed with input: %s \n", input);
+                ret = 0;
+            }
+            return ret;
 
-	    if (ret == 0) {
-                fprintf(stdout, "Test case successful! \n");
-		return 1;
-	    } else if (ret < 0) {
-	        fprintf(stdout, "[ERROR] - Some error occured while executing this test-case! \n");
-		return 0;
-	    } else {
-                fprintf(stdout, "Input-output doesn't match! \n");
-		return 0;
-	    }
-
-            break;
     }
 }    
 
-int test_loop(int parent_child_fd, int child_parent_fd, char * input, char * output, int should_fail) {
+int test_loop(int parent_child_fd, int child_parent_fd, char * input) {
 
     int ret_state = 0;
     fprintf(stdout, "reached test_loop state. \n");
-    dprintf(parent_child_fd, "%s", input);
-    
+    dprintf(parent_child_fd, "%s\n", input);
+
+    fsync(parent_child_fd);
+
     int retpc = close(parent_child_fd);
     if (retpc != 0) {
-	fprintf(stderr, "failed to close file-descriptor! \n");
-	ret_state = -1;
+        fprintf(stderr, "failed to close file-descriptor! \n");
+        ret_state = -1;
     }
- 
 
     int size = 512;    
     char line[size];
@@ -165,26 +194,17 @@ int test_loop(int parent_child_fd, int child_parent_fd, char * input, char * out
 
     ssize_t ret = read(child_parent_fd, line, size);
     if (ret < 0) {
-	fprintf(stderr, "error while reading from child! \n");
-	ret_state = -1;
+        fprintf(stderr, "error while reading from child! \n");
+        ret_state = -1;
     }
 
-    fprintf(stdout, "input: <%s>, should-output: <%s>, is-output: <%s>\n", input, output, line);
-    
-    int len_should = strlen(output);
-    int len_is = strlen(line);
-    if (len_should != len_is) {
-        ret_state = 1;
-    }
-    if (strcmp(output, line) != 0) {
-	ret_state = 1;
-    }
+    fprintf(stdout, "input: <%s>, is-output: <%s>\n", input, line);
 
     int retcp = close(child_parent_fd);
     if (retcp != 0) {
         fprintf(stderr, "failed to close file-stream! \n");
-	ret_state = -1;
+        ret_state = -1;
     }
- 
+
     return ret_state;
 }
